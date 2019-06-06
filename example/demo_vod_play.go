2f0c2a51680535afeb085416b7a1beae50de4e1c
黄伟俊 <huangweijun@bytedance.com>
2019-06-06 17:25:38 +0800

    add upload api
    Change-Id: I93bced43d03c0576803c13c30b52cfc1dab1b0cb

--- example/demo_vod_play.go
+++ example/demo_vod_play.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/url"
 
 	"github.com/TTvcloud/vcloud-sdk-golang/base"
@@ -10,16 +11,63 @@ import (
 	"github.com/TTvcloud/vcloud-sdk-golang/service/vod"
 )
 
-func main() {
-	query := url.Values{}
-	query.Set("video_id", "your vid")
+const spaceName = "your-space-name"
 
+func main() {
 	vod.DefaultInstance.SetCredential(base.Credentials{
 		AccessKeyID:     "your ak",
 		SecretAccessKey: "your sk"})
 
+	query := url.Values{}
+	query.Set("video_id", "your vid")
+
 	resp, code, _ := vod.DefaultInstance.GetPlayInfo(query)
+	fmt.Printf("resp:%+v code:%d\n", resp, code)
 	fmt.Println(code)
 	b, _ := json.Marshal(resp)
 	fmt.Println(string(b))
+	// uploadAll()
+	// uploadVideoByUrl()
+}
+
+func uploadVideoByUrl() {
+	params := vod.UploadVideoByUrlParams{
+		SpaceName:  spaceName,
+		Format:     vod.MP4,
+		SourceUrls: []string{"video-url"},
+		Extra:      "xxx",
+	}
+	resp, err := vod.DefaultInstance.UploadVideoByUrl(params)
+	if err != nil {
+		fmt.Printf("err:%s\n")
+	}
+	if resp.ResponseMetadata.Error != nil {
+		fmt.Println(resp.ResponseMetadata.Error)
+		return
+	}
+	fmt.Println(resp.Result)
+}
+
+func uploadAll() {
+	snapShotFunc := vod.Function{Name: "Snapshot", Input: vod.SnapshotInput{SnapshotTime: 2.3}}
+	getMetaFunc := vod.Function{Name: "GetMeta"}
+
+	resp, err := upload(spaceName, "path-to-video", vod.VIDEO, getMetaFunc, snapShotFunc)
+	fmt.Printf("resp:%+v err:%s", resp, err)
+	resp, err = upload(spaceName, "path-to-img", vod.IMAGE, snapShotFunc)
+	fmt.Printf("resp:%+v err:%s", resp, err)
+	resp, err = upload(spaceName, "path-to-obj", vod.OBJECT, vod.Function{Name: "GetMeta"}, snapShotFunc)
+	fmt.Printf("resp:%+v err:%s", resp, err)
+}
+
+func upload(spaceName string, filePath string, fileType vod.FileType, funcs ...vod.Function) (*vod.CommitUploadResp, error) {
+	dat, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	rsp, err := vod.DefaultInstance.Upload(dat, spaceName, fileType, funcs...)
+	if err != nil {
+		return nil, err
+	}
+	return rsp, nil
 }
