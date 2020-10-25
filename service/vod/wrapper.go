aa585b02b59412f94c4cb4554dbde95f4d1160e6
leiyixing <leiyixing@bytedance.com>
2020-10-25 12:19:59 +0800

    update for pb vod params

--- service/vod/wrapper.go
+++ service/vod/wrapper.go
@@ -5,64 +5,57 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"github.com/TTvcloud/vcloud-sdk-golang/models"
+	"github.com/golang/protobuf/jsonpb"
 	"hash/crc32"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"net/url"
-	"os"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/TTvcloud/vcloud-sdk-golang/base"
 
 	"github.com/pkg/errors"
-
-	jsoniter "github.com/json-iterator/go"
 )
 
 //GetPlayInfo 获取播放信息
-func (p *Vod) GetPlayInfo(video GetPlayInfoReq) (*GetPlayInfoResp, int, error) {
-	vid := video.Vid
+func (p *Vod) GetPlayInfo(video *models.VodGetPlayInfoRequest) (*models.VodGetPlayInfoResponse, int, error) {
+	vid := video.GetVid()
 	if len(vid) == 0 {
-		return nil, http.StatusBadRequest, errors.New("Empty Vid")
+		return nil, http.StatusBadRequest, errors.New(string(invalidParameter))
 	}
 	query := url.Values{}
-	query.Set("Vid", video.Vid)
-	query.Set("Base64", strconv.FormatInt(video.Base64, 10))
-	query.Set("Ssl", strconv.FormatInt(video.Ssl, 10))
+	query.Set("Vid", vid)
+	query.Set("Base64", video.GetBase64())
+	query.Set("Ssl", video.GetSsl())
 
-	if len(video.FormatType) > 0 {
-		query.Set("Format", video.FormatType)
+	if len(video.GetFormat()) > 0 {
+		query.Set("Format", video.GetFormat())
 	}
-	if len(video.CodecType) > 0 {
-		query.Set("Codec", video.CodecType)
+	if len(video.GetCodec()) > 0 {
+		query.Set("Codec", video.GetCodec())
 	}
-	if len(video.Definition) > 0 {
-		query.Set("Definition", video.Definition)
+	if len(video.GetDefinition()) > 0 {
+		query.Set("Definition", video.GetDefinition())
 	}
-	if len(video.Watermark) > 0 {
-		query.Set("Watermark", video.Watermark)
+	if len(video.GetLogoType()) > 0 {
+		query.Set("LogoType", video.GetLogoType())
 	}
-	if len(video.StreamType) > 0 {
-		query.Set("StreamType", video.StreamType)
+	if len(video.GetFileType()) > 0 {
+		query.Set("FileType", video.GetFileType())
 	}
 
 	respBody, status, err := p.Query("GetPlayInfo", query)
 	if err != nil {
 		return nil, status, err
 	}
-
-	output := new(GetPlayInfoResp)
-	jsonTag := os.Getenv("JSON_FORMATTER")
-	switch jsonTag {
-	case base.JSON_FORMATTER_JSONITER:
-		jsonFormatter := jsoniter.ConfigCompatibleWithStandardLibrary
-		err = jsonFormatter.Unmarshal(respBody, output)
-	default:
-		err = json.Unmarshal(respBody, output)
+	output := &models.VodGetPlayInfoResponse{}
+	unmarshaler := jsonpb.Unmarshaler{
+		AllowUnknownFields: true,
 	}
+	err = unmarshaler.Unmarshal(bytes.NewReader(respBody), output)
 	if err != nil {
 		return nil, status, err
 	} else {
@@ -71,24 +64,24 @@ func (p *Vod) GetPlayInfo(video GetPlayInfoReq) (*GetPlayInfoResp, int, error) {
 }
 
 //GetOriginVideoPlayInfo 获取原片播放信息
-func (p *Vod) GetOriginVideoPlayInfo(req GetOriginVideoPlayInfoReq) (*GetOriginVideoPlayInfoResp, int, error) {
+func (p *Vod) GetOriginVideoPlayInfo(req *models.VodGetOriginalPlayInfoRequest) (*models.VodGetOriginalPlayInfoResponse, int, error) {
+	vid := req.GetVid()
+	if len(vid) == 0 {
+		return nil, http.StatusBadRequest, errors.New(string(invalidParameter))
+	}
 	query := url.Values{}
-	query.Set("Vid", req.Vid)
-	query.Set("Base64", strconv.FormatInt(req.Base64, 10))
-	query.Set("Ssl", strconv.FormatInt(req.Ssl, 10))
+	query.Set("Vid", vid)
+	query.Set("Base64", req.GetBase64())
+	query.Set("Ssl", req.GetSsl())
 	respBody, status, err := p.Query("GetOriginVideoPlayInfo", query)
 	if err != nil {
 		return nil, status, err
 	}
-	output := new(GetOriginVideoPlayInfoResp)
-	jsonTag := os.Getenv("JSON_FORMATTER")
-	switch jsonTag {
-	case base.JSON_FORMATTER_JSONITER:
-		jsonFormatter := jsoniter.ConfigCompatibleWithStandardLibrary
-		err = jsonFormatter.Unmarshal(respBody, output)
-	default:
-		err = json.Unmarshal(respBody, output)
+	output := &models.VodGetOriginalPlayInfoResponse{}
+	unmarshaler := jsonpb.Unmarshaler{
+		AllowUnknownFields: true,
 	}
+	err = unmarshaler.Unmarshal(bytes.NewReader(respBody), output)
 	if err != nil {
 		return nil, status, err
 	} else {
@@ -96,33 +89,33 @@ func (p *Vod) GetOriginVideoPlayInfo(req GetOriginVideoPlayInfoReq) (*GetOriginV
 	}
 }
 
-
-//GetRedirectPlayUrl get redirected playback addres
-func (p *Vod) GetRedirectPlayUrl(req RedirectPlayReq) (string, error) {
-	query := url.Values{}
-	if req.Vid == "" {
-		return "", errors.New("Vid not set")
-	}
-	query.Add("Vid", req.Vid)
-	if len(req.Definition) > 0 {
-		query.Set("Definition", req.Definition)
-	}
-	if req.Watermark != "" {
-		query.Add("Watermark", req.Watermark)
-	}
-	if req.Expires != "" {
-		query.Add("X-Amz-Expires", req.Expires)
-	}
-
-	token, err := p.GetSignUrl("RedirectPlay", query)
-	if err != nil {
-		return "", err
-	}
-
-	apiInfo := p.ApiInfoList["RedirectPlay"]
-	url := fmt.Sprintf("http://%s%s?%s", p.ServiceInfo.Host, apiInfo.Path, token)
-	return url, nil
-}
+//
+////GetRedirectPlayUrl get redirected playback addres
+//func (p *Vod) GetRedirectPlayUrl(req RedirectPlayReq) (string, error) {
+//	query := url.Values{}
+//	if req.Vid == "" {
+//		return "", errors.New("Vid not set")
+//	}
+//	query.Add("Vid", req.Vid)
+//	if len(req.Definition) > 0 {
+//		query.Set("Definition", req.Definition)
+//	}
+//	if req.Watermark != "" {
+//		query.Add("Watermark", req.Watermark)
+//	}
+//	if req.Expires != "" {
+//		query.Add("X-Amz-Expires", req.Expires)
+//	}
+//
+//	token, err := p.GetSignUrl("RedirectPlay", query)
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	apiInfo := p.ApiInfoList["RedirectPlay"]
+//	url := fmt.Sprintf("http://%s%s?%s", p.ServiceInfo.Host, apiInfo.Path, token)
+//	return url, nil
+//}
 
 func (p *Vod) StartTranscode(req *StartTranscodeRequest) (*StartTranscodeResp, error) {
 	query := url.Values{
