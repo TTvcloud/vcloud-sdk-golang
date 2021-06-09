4f3388ccf4a30d0d649d124ff4eb7bb3ea7b15c4
赵洪达 <zhaohongda@bytedance.com>
2021-06-09 15:21:22 +0800

    improve

--- service/imagex/ocr.go
+++ service/imagex/ocr.go
@@ -11,10 +11,12 @@ const LicenseScene = "license"
 func (c *ImageXClient) GetImageOCR(param *GetImageOCRParam) (*GetImageOCRResult, error) {
 	u := url.Values{}
 	c.ServiceInfo.Header.Add("X-Top-Account-Id", param.AccountId)
-	c.ServiceInfo.Header.Add("Content-type", "application/octet-stream")
 	u.Set("Scene", param.Scene)
 	u.Set("ServiceId", param.ServiceId)
 	u.Set("StoreUri", param.StoreUri)
+	if param.StoreUri == "" {
+		c.ServiceInfo.Header.Add("Content-type", "application/octet-stream")
+	}
 	data, _, err := c.PostWithBody("GetImageOCR", u, bytes.NewReader(param.Image))
 	if err != nil {
 		return nil, fmt.Errorf("fail to request api GetImageOCR, %v\n", err)
